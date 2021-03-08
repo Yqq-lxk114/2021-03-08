ee705a043a5e27a52009cb05b60ec2bfb38268a9
Yqq-lxk114 <1957902981@qq.com>
2021-03-07 22:49:47 -0500

    1.c

---
+++ 1.c
@@ -0,0 +1,22 @@
+/*================================================================
+*   Copyright (C) 2021 hqyj Ltd. All rights reserved.
+*   
+*   文件名称：1.c
+*   创 建 者：yqq
+*   创建日期：2021年03月07日
+*   描    述：
+*
+================================================================*/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("111111\n");
+	printk("this is demo_init\n");
+	
+	return 0;
+}
+
